3b5910714864a3a43d0eba5c0190a83c48bd5363
Murad <muradav@code.edu.az>
2024-01-27 19:49:58 +0400

    Add project files.

---
+++ DapperDemo/Repository/ICompanyRepository.cs
@@ -0,0 +1,16 @@
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace DapperDemo.Repository
+{
+    public interface ICompanyRepository
+    {
+        Company Find(int id);
+        List<Company> GetAll();
+
+        Company Add(Company company);
+        Company Update(Company company);
+
+        void Remove(int id);
+    }
+}
