8c5c5229d038b7efc3e61a2a88c3878f5bd2b6e3
Murad <muradav@code.edu.az>
2024-01-30 00:45:09 +0400

    feat: Dapper Contrib

--- DapperDemo/Models/Company.cs
+++ DapperDemo/Models/Company.cs
@@ -1,15 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Dapper.Contrib.Extensions;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DapperDemo.Models
 {
+    [Dapper.Contrib.Extensions.Table("Companies")]
     public class Company
     {
+        [Key]
         public int CompanyId { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        
+        [Write(false)]
         public List<Employee>? Employees { get; set; }
     }
 }
