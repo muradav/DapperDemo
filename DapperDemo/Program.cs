f164be3e868e1e599efd72e5fdc47ccc750e6520
Murad <muradav@code.edu.az>
2024-01-30 00:21:05 +0400

    feat: Stored Procedures in Dapper

--- DapperDemo/Program.cs
+++ DapperDemo/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
      options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepositorySP>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddControllersWithViews();
 
